53ad4b4fa71b8b598a979b3c2b936e8f1832ac7f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-18 05:22:52 +0530

    Added: Code to park a car when arrives

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ParkingLot {
+    Car car = new Car();
+    Map<String, Car> carMap = new HashMap<>();
+    int capacity  = 10;
+
     public boolean park(Car car) {
         return true;
     }
+
+
 }
