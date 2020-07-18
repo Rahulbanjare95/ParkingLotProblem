e12ae940d5c7f6a6ecc1b3b814da97b5f1b8f1b1
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-18 09:46:06 +0530

    Added: Test case to check when parking lot is full should throw exception

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,5 +1,4 @@
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ParkingLotTest {
@@ -12,16 +11,45 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenCarWithDetails_whenParked_ShouldReturnTrue() {
-        boolean b = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
-        Assert.assertTrue(b);
+    public void givenOneCarWithDetails_whenParked_ShouldReturnOneCar() throws ParkingLotException {
+        int i = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
+        Assert.assertEquals(1,i);
     }
 
     @Test
-    public void givenCarWithDetails_whenUnparked_ShouldReturnTrue() {
-        boolean b = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
-        boolean isUnpark = parkingLot.unParkCar("CG11M7393");
-        Assert.assertTrue(isUnpark);
+    public void givenCarWithDetails_whenUnParked_ShouldReturnTrue() {
+        try {
+        int i = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
+        boolean isUnPark= parkingLot.unParkCar("CG11M7393");
+        Assert.assertTrue(isUnPark);
+        } catch (ParkingLotException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenCarDetailsIncorrect_WhenUnParked_ShouldThrowException() {
+        try{
+            int i  = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
+            boolean isUnPark = parkingLot.unParkCar("CG11M793");
+        }catch (ParkingLotException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenMultipleCarDetails_whenTriedToExceedParkingLimit_ShouldThrowException() throws ParkingLotException {
+        int b = parkingLot.parkWithDetails("CG11M7393", "Hyundai Verna");
+        int b1 = parkingLot.parkWithDetails("KA01B1212", "BMW 720D");
+        int b2 = parkingLot.parkWithDetails("DL03C0003", "Mercedes-Benz S-Class");
+        int b3 = parkingLot.parkWithDetails("MH04CD1011", "Mercedes-Benz S-Class");
+        int b4 = parkingLot.parkWithDetails("KA07B", "Mercedes-Benz S-Class");
+        try {
+            int parkingAllotmentDetails = parkingLot.getParkingAllotmentDetails();
+            Assert.assertEquals(5,parkingAllotmentDetails);
+        } catch (ParkingLotException e) {
+            e.printStackTrace();
+        }
 
     }
 }
