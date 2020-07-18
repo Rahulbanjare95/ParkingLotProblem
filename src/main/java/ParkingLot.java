1df321b4b7f77b9694a35e22037f662a71740fb0
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-18 09:52:27 +0530

    Added: Code to get parking details for number of cars parked

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -4,26 +4,37 @@ public class ParkingLot {
     Car car = new Car();
 
     Map<String, Car> carMap= new HashMap<>();
-    int capacity  = 3;
 
     public boolean park(Car car) {
         return true;
     }
+    String registration;
+    String model;
+    int capacity  = 3;
+    public int parkWithDetails(String registration, String model) throws ParkingLotException {
 
-
-    public boolean parkWithDetails(String registration, String model) {
-        if (carMap.size() < capacity){
+        if (carMap.size() <= capacity){
             carMap.put(registration, new Car(registration, model));
-            return true;
+            return carMap.size();
         }
-        return false;
+        throw new ParkingLotException("ParkingLot Full", ParkingLotException.ExceptionType.PARKING_LOT_FULL);
     }
-    public boolean unParkCar (String registration){
-
+    public boolean unParkCar (String registration) throws ParkingLotException {
         if (carMap.containsKey(registration)) {
             carMap.remove(registration);
             return true;
         }
-            return false;
+         throw  new ParkingLotException("No such Car exist",ParkingLotException.ExceptionType.WRONG_DETAILS);
+    }
+
+    public int getParkingAllotmentDetails() throws ParkingLotException {
+        int i = this.parkWithDetails(registration, model);
+        if(i<capacity){
+            return i;
         }
+        if (i == capacity){
+            return capacity;
+        }
+        throw new ParkingLotException("ParkingLot Full", ParkingLotException.ExceptionType.PARKING_LOT_FULL);
+    }
 }
