91a6e8fdb474d267ac3b760ce71ed107c8109b01
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-18 08:34:47 +0530

    Added: Test case to check given registration number should unpark the car

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -2,24 +2,26 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ParkingLotTest {
+    ParkingLot parkingLot = new ParkingLot();
 
     @Test
     public void givenCar_whenParked_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
         boolean isParked = parkingLot.park(new  Car());
         Assert.assertTrue(isParked);
     }
 
     @Test
     public void givenCarWithDetails_whenParked_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
         boolean b = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
         Assert.assertTrue(b);
     }
 
+    @Test
+    public void givenCarWithDetails_whenUnparked_ShouldReturnTrue() {
+        boolean b = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
+        boolean isUnpark = parkingLot.unParkCar("CG11M7393");
+        Assert.assertTrue(isUnpark);
 
+    }
 }
