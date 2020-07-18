bbb80a51343d1f2f0db8a988d760ced20374aa22
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-18 07:39:14 +0530

    Added: Test case to check given registration number car is parked or not

--- src/test/java/ParkingLotTest.java
+++ src/test/java/ParkingLotTest.java
@@ -1,11 +1,25 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotTest {
+
     @Test
     public void givenCar_whenParked_ShouldReturnTrue() {
         ParkingLot parkingLot = new ParkingLot();
         boolean isParked = parkingLot.park(new  Car());
         Assert.assertTrue(isParked);
     }
+
+    @Test
+    public void givenCarWithDetails_whenParked_ShouldReturnTrue() {
+        ParkingLot parkingLot = new ParkingLot();
+        boolean b = parkingLot.parkWithDetails("CG11M7393", "HyundaiVerna");
+        Assert.assertTrue(b);
+    }
+
+
 }
