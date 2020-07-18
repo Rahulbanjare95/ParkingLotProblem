1adee231673072512f636d69a24061eca5aba7bd
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-18 10:00:52 +0530

    Added: Exception to package  exception

---
+++ src/main/java/exception/ParkingLotException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class ParkingLotException extends Exception {
+    public enum ExceptionType {
+        PARKING_LOT_FULL, WRONG_DETAILS
+    }
+
+    public ExceptionType type;
+
+    public ParkingLotException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
