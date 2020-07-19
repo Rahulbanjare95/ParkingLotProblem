d7d15413561022c825f7d683756e4ff91a0d2761
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-20 01:02:19 +0530

    Added: Enum to a package

--- src/main/java/service/ParkingLotControllers.java
+++ src/main/java/service/ParkingLotControllers.java
@@ -1,11 +1,9 @@
 package service;
 
 public enum ParkingLotControllers {
-    AIRPORT_SECURITY(false), PARKING_LOT_OWNER(false);
+    AIRPORT_SECURITY(true), PARKING_LOT_OWNER(false);
 
     public boolean isParkingLotFull;
-    public boolean isSpaceAvailableInParking;
-
     ParkingLotControllers(boolean isParkingLotFull) {
         this.isParkingLotFull = isParkingLotFull;
     }
