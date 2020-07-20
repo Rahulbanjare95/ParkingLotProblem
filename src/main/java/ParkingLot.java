e8973c827bbd514b2b3a47a943b70c2fa96454c0
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-20 10:26:04 +0530

    Refactored: Removed unused imports

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -1,9 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
 
 public class ParkingLot {
     Car car = new Car();
-    Map<String, Car> carMap = new HashMap<>();
+
     public boolean park(Car car) {
         return true;
     }
