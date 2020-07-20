feafda045379db82eea7ab7b8ca18e1294290182
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-20 09:22:01 +0530

    Added: Car with its attributes

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,12 @@
 public class Car {
+    String registration;
+    String model;
+
+    public Car() {
+    }
+
+    public Car(String registration, String model) {
+        this.registration = registration;
+        this.model = model;
+    }
 }
