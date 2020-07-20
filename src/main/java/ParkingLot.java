827cb4cf35a1c7560ca40e42e62e33027e2950e6
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-20 10:24:53 +0530

    Refactored: Removed unused variables

--- src/main/java/ParkingLot.java
+++ src/main/java/ParkingLot.java
@@ -4,8 +4,6 @@ import java.util.Map;
 public class ParkingLot {
     Car car = new Car();
     Map<String, Car> carMap = new HashMap<>();
-    int capacity  = 10;
-
     public boolean park(Car car) {
         return true;
     }
